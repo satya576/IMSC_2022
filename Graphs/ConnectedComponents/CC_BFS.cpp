1e1c369a402b0d196ff548d972cbf64b2dcb17fa
Satyaprasad <satya576@gmail.com>
2022-10-22 22:58:21 +0530

    usual update

--- Graphs/ConnectedComponents/CC_BFS.cpp
+++ Graphs/ConnectedComponents/CC_BFS.cpp
@@ -3,21 +3,21 @@ using namespace std;
 
 int vis[100][100] = {0};
 
-void BFS(int i, int j, vector<string> &Grid){
+void BFS(int i, int j, int n, int m, vector<string> &Grid){
     vis[i][j] = 1;
     queue<pair<int, int>> Q;
     Q.push({i,j});
     int dx[] = {-1, -1, 0,1, 1,1,0,-1};
     int dy[] = {0,1,1,1,0,-1,-1,-1};
-    int m = Grid.size();
-    int n = Grid[0].size();
+    
     while(!Q.empty()){
         int r = Q.front().first;
         int c = Q.front().second;
+        Q.pop();
         for( int k = 0 ; k < 8 ; k++){
             int nr = r + dx[k];
             int nc = c + dy[k];
-            if(nr >= 0 && nr < m && nc >= 0 && nc < n && Grid[nr][nc] == '@' && vis[nr][nc] == 0){
+            if(nr >= 0 && nr < n && nc >= 0 && nc < m && Grid[nr][nc] == '@' && vis[nr][nc] == 0){
                 vis[nr][nc] = 1;
                 Q.push({nr, nc});
             }
@@ -41,17 +41,17 @@ int main(){
         cin>>str;
         Grid.push_back(str);
     }
-    
+    //cout<<Grid[0]<<"\n";
     int count = 0;
     for(int i = 0 ; i < n ; i++){
         for(int j = 0 ; j < m ; j++){
             if(Grid[i][j] == '@' && vis[i][j] == 0 ){
                 count++;
-                BFS(i, j, Grid);
+                BFS(i, j, n, m, Grid);
             }
         }
     }
 
-    cout<<count<<"\n";
+   cout<<count<<"\n";
 
 }
